bf89d2aad0977bc70b9a06e0d4638935b4020d28
Chris Wood <118241499+Cwood00@users.noreply.github.com>
2023-10-10 19:54:59 -0500

    Added Card class

---
+++ src/main/java/Card.java
@@ -0,0 +1,8 @@
+public class Card {
+    public String suite;
+    public int value;
+    public Card(String theSuite, int theValue){
+        this.suite = theSuite;
+        this.value = theValue;
+    }
+}
