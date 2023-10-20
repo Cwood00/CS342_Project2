a4de80abf880e1e5ca2a464873d6c9ea357bafdd
Chris Wood <cwood@biped.us>
2023-10-20 00:14:10 -0500

    Started work on play scene

--- src/main/java/BaccaratGame.java
+++ src/main/java/BaccaratGame.java
@@ -8,58 +8,84 @@ import javafx.animation.SequentialTransition;
 import javafx.application.Application;
 
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
 import javafx.util.Duration;
+import org.w3c.dom.Text;
+
+import java.util.ArrayList;
 
 
 public class BaccaratGame extends Application {
+	private ArrayList<Card> playerHand;
+	private ArrayList<Card> bankerHand;
+	private BaccaratDealer theDealer;
+	private BaccaratGameLogic gameLogic;
+	private double currentBet;
+	private double totalWinnings;
+	private String betPlacedOn;
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		launch(args);
 	}
 
 	//feel free to remove the starter code from this method
 	@Override
 	public void start(Stage primaryStage) throws Exception {
-		// TODO Auto-generated method stub
-		primaryStage.setTitle("Welcome to JavaFX");
-		
-		 Rectangle rect = new Rectangle (100, 40, 100, 100);
-	     rect.setArcHeight(50);
-	     rect.setArcWidth(50);
-	     rect.setFill(Color.VIOLET);
+		primaryStage.setTitle("Baccarat");
 
-	     RotateTransition rt = new RotateTransition(Duration.millis(5000), rect);
-	     rt.setByAngle(270);
-	     rt.setCycleCount(4);
-	     rt.setAutoReverse(true);
-	     SequentialTransition seqTransition = new SequentialTransition (
-	         new PauseTransition(Duration.millis(500)),
-	         rt
-	     );
-	     seqTransition.play();
-	     
-	     FadeTransition ft = new FadeTransition(Duration.millis(5000), rect);
-	     ft.setFromValue(1.0);
-	     ft.setToValue(0.3);
-	     ft.setCycleCount(4);
-	     ft.setAutoReverse(true);
+		//-----Start scene-----//
+		//TODO create start scene
 
-	     ft.play();
-	     BorderPane root = new BorderPane();
-	     root.setCenter(rect);
-	     
-	     Scene scene = new Scene(root, 700,700);
-		 primaryStage.setScene(scene);
-		 primaryStage.show();
+		//-----Betting scene-----//
+		//TODO create betting scene
 
-		Card newCard = new Card("Heart", 5);
-		
+		//-----Play scene-----//
+		BorderPane playSceneRoot = new BorderPane();
+
+		TextField playerCardHeader = new TextField("Player");
+		TextField bankerCardHeader = new TextField("Banker");
+		TextField playerCardFooter = new TextField("Score: ");
+		TextField bankerCardFooter = new TextField("Score: ");
+		playerCardHeader.setEditable(false);
+		bankerCardHeader.setEditable(false);
+		playerCardFooter.setEditable(false);
+		bankerCardFooter.setEditable(false);
+
+		HBox playerHandBox = new HBox();
+		HBox bankerHandBox = new HBox();
+
+		VBox playerCardBox = new VBox(playerCardHeader, playerHandBox, playerCardFooter);
+		VBox bankerCardBox = new VBox(bankerCardHeader, bankerHandBox, bankerCardFooter);
+
+		TextField playerWinningsTextField = new TextField("Player total winnings: $" + totalWinnings);
+		TextField currentBetTextField = new TextField("Current Bet: $" + currentBet);
+		Button dealAndPlayAgainButton = new Button("Deal");
+		TextField resultsTextField = new TextField();
+		playerWinningsTextField.setEditable(false);
+		currentBetTextField.setEditable(false);
+		resultsTextField.setEditable(false);
+
+		VBox centralElements = new VBox(playerWinningsTextField, currentBetTextField, dealAndPlayAgainButton, resultsTextField);
+
+		playSceneRoot.setLeft(playerCardBox);
+		playSceneRoot.setRight(bankerCardBox);
+		playSceneRoot.setCenter(centralElements);
+
+		Scene playScene = new Scene(playSceneRoot, 1080,720);
+
+		primaryStage.setScene(playScene);//TODO change to start scene when implemented
+		primaryStage.show();
+	}
+
+	public double evaluateWinnings(){
+		return 0.0;//TODO implement this function
 	}
 
 }
